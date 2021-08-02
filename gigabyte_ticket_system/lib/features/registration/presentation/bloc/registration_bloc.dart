d99db6b6bfdf4a2103b32d7e7a6234e1f3c2d32e
jake1808 <51802968+jake1808@users.noreply.github.com>
2021-08-02 12:59:18 +0300

    registration data feature implemented

--- gigabyte_ticket_system/lib/features/registration/presentation/bloc/registration_bloc.dart
+++ gigabyte_ticket_system/lib/features/registration/presentation/bloc/registration_bloc.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:gigabyte_ticket_system/features/registration/data/models/users.dart';
 part 'registration_event.dart';
 part 'registration_state.dart';
+
 class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
   RegistrationBloc() : super(RegistrationInitial());
   @override
